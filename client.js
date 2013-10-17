511487eb4c8b3fb26529b6ee49134c2419c7fe5d
Yarek T <yarekt@gmail.com>
2013-10-17 21:15:20 +0100

    Client imrpovements

--- client.js
+++ client.js
@@ -1,5 +1,17 @@
 var rpc = require('jsonrpc2');
-var client = new rpc.Client(8000, 'localhost');
-client.call('print', ['Hello2'], function(err, result) {
+var client = new rpc.Client(8000, 'lcd.yarekt.co.uk');
+var send = function(message) {
+message += '';
+client.call('print', [message], function(err, result) {
     console.log(result);
+});
+}
+
+var readline = require('readline');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.on("line", function(text) {
+    send(text);
 });
